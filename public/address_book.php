dec31f827b4205585c13787a2cbc0b40bc92442c
Kris C <kris541@hotmail.com>
2014-09-11 08:59:49 -0500

    added read and write functions to address book along with remove link

--- public/address_book.php
+++ public/address_book.php
@@ -3,24 +3,44 @@
 
 define('FILENAME', 'address_book.csv');
 
-$address_book = [
-    ['The White House', '1600 Pennsylvania Avenue NW', 'Washington', 'DC', '20500'],
-    ['Marvel Comics', 'P.O. Box 1527', 'Long Island City', 'NY', '11101'],
-    ['LucasArts', 'P.O. Box 29901', 'San Francisco', 'CA', '94129-0901']
-];
-
-
 
 function save_csv($address_book, $filename = FILENAME)
 {
     $handle = fopen($filename, 'w');
-    foreach($address_book as $fields) {
+    //$fields is an array of the row of data entered//
+    foreach($address_book as $fields) 
+    {
         fputcsv($handle, $fields);
     }    
     fclose($handle);
     
 }
 
+function read_file($filename = FILENAME)
+{
+    $address_book =[];
+    $handle = fopen($filename, 'r');
+    while(!feof($handle)) 
+    {   
+        $fields = fgetcsv($handle);
+        if(!empty($fields)) 
+        {
+            $address_book[] = $fields;
+        }
+    }
+    fclose($handle);
+    return $address_book;
+    
+}
+
+
+
+
+
+
+// $address_book =[];
+
+$address_book = read_file();
 
 if (!empty($_POST['name']) && !empty($_POST['address']) && !empty($_POST['city']) && !empty($_POST['state']) && !empty($_POST['zip'])) 
 {
@@ -36,8 +56,32 @@ if (!empty($_POST['name']) && !empty($_POST['address']) && !empty($_POST['city']
     array_push($address_book, $new_address);
     save_csv($address_book);
     
+} else 
+    {
+        foreach ($_POST  as $key => $value) 
+        {
+            if (empty($value)) 
+            {
+                echo ($key) .  " is required ";
+            }
+        }   
+    }
+
+
+
+if (isset($_GET['remove'])) 
+{
+    // Define variable $keyToRemove according to value
+    $keyToRemove = $_GET['remove'];
+    // Remove item from array according to key specified
+    unset($address_book[$keyToRemove]);
+    // Numerically reindex values in array after removing item
+    $address_book = array_values($address_book);
+    // Save to file
+    save_csv($address_book);
 }
-   
+
+
 
 
 
@@ -48,10 +92,25 @@ if (!empty($_POST['name']) && !empty($_POST['address']) && !empty($_POST['city']
     <title>Address Book</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    <style>
-        
+    <style type="text/css">
 
-
+    a:link {
+        color: black;
+    }
+    h1 {
+        text-align: center;
+    }
+    body {
+        background-image: url(/img/brown.jpg);
+        background-size: cover;
+        background-repeat: no-repeat; 
+        text-decoration: bold;      
+    }
+    .table-bordered {
+        border-color: red;
+        border: 4px solid;
+    }
+    
     </style>
 </head>
 <body>
@@ -59,11 +118,12 @@ if (!empty($_POST['name']) && !empty($_POST['address']) && !empty($_POST['city']
     <?php var_dump($_GET); ?>
     <h2>$_POST</h2>
     <?php var_dump($_POST); ?> -->
-    
+
+    <div id="header">
     <h1>ADDRESS BOOK</h1>
+    </div>
 
-    <table class="table table-striped">
-
+    <table class="table table-bordered">
         <tr>
             <th>Name</th>
             <th>Address</th>
@@ -71,19 +131,22 @@ if (!empty($_POST['name']) && !empty($_POST['address']) && !empty($_POST['city']
             <th>State</th>
             <th>Zip</th>
             <th>Phone</th>
+            <th>Remove</th>
         </tr>
 
-        <? foreach ($address_book as $fields) : ?>
+        <? foreach ($address_book as $key => $fields) : ?>
         <tr>
-            <? foreach($fields as$key => $value) : ?>
+            <? foreach($fields as $value) : ?>
                 <td><?= htmlspecialchars(strip_tags($value)); ?></td>
             <?endforeach; ?>
+            <td><a href="<?= '?remove=' . $key; ?>">REMOVE</a></td>
         </tr>
         <? endforeach; ?>  
              
     </table>
 
 
+        
 
 
     <form method="POST" action="address_book.php">
